d3bef673269c376a8682e3b9d5309daf36e36e46
Saqoosha <a@saqoo.sh>
2015-10-22 12:59:12 +0900

    [exp/04] Deform!

--- experiments/04 - Face Deformation/src/main.js
+++ experiments/04 - Face Deformation/src/main.js
@@ -47,6 +47,7 @@ class App {
     this.animate();
   }
 
+
   initScene() {
     this.camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 3000);
     this.camera.position.z = 500;
@@ -59,7 +60,7 @@ class App {
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-    let container = document.querySelector('.container');
+    const container = document.querySelector('.container');
     container.appendChild(this.renderer.domElement);
 
     window.addEventListener('resize', this.onResize.bind(this));
@@ -70,20 +71,21 @@ class App {
     // window.addEventListener('keydown', this.onKeyDown.bind(this));
   }
 
+
   initObjects() {
     this.root = new THREE.Object3D();
     this.root.scale.set(90, 90, 90);
     this.scene.add(this.root);
 
-    let geometry = new THREE.JSONLoader().parse(require('json!./face.json')).geometry;
+    const geometry = new THREE.JSONLoader().parse(require('json!./face.json')).geometry;
     geometry.computeBoundingBox();
     this.face = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({transparent: true, opacity: 0.3, wireframe: true}));
     this.face.position.copy(geometry.boundingBox.center().negate());
     this.root.add(this.face);
 
     this.featurePointIndices = [];
-    require('json!./fp.json').forEach(pa => {
-      let p = new THREE.Vector3(pa[0], pa[1], pa[2]);
+    require('json!./fp.json').forEach((pa) => {
+      const p = new THREE.Vector3(pa[0], pa[1], pa[2]);
       this.featurePointIndices.push(p.length() > 0 ? this.findNearestIndex(geometry.vertices, p) : -1);
     });
 
@@ -121,8 +123,46 @@ class App {
 
     // console.log(this.featurePointIndices[33]);
     // this.calcWeightForNode(this.nodes[408]);
-    this.nodes.forEach(node => this.calcWeightForNode(node));
+    this.nodes.forEach((node) => this.calcWeightForNode(node));
+
+    // let cube = new THREE.BoxGeometry(0.05, 0.05, 0.05);
+    // this.nodes.forEach(node => {
+    //   node.weights.forEach(w => {
+    //     if (w.i == 1) {
+    //       let material = new THREE.MeshBasicMaterial({color: 0xff0000, transparent: true, opacity: 0.7, depthTest: false});
+    //       let mesh = new THREE.Mesh(cube, material);
+    //       mesh.position.copy(node.position);
+    //       mesh.scale.set(w.w, w.w, w.w);
+    //       this.face.add(mesh);
+    //     }
+    //   });
+    // });
+
+    let cube = new THREE.BoxGeometry(0.03, 0.03, 0.03);
+    this.featurePoints = this.featurePointIndices.map((i) => {
+      if (i < 0) return;
+      let material = new THREE.MeshBasicMaterial({color: 0xff0000, transparent: true, opacity: 0.7, depthTest: false});
+      material.color.setHSL(Math.random(), 0.7, 0.5);
+      let mesh = new THREE.Mesh(cube, material);
+      mesh.vertexIndex = i;
+      mesh.position.copy(geometry.vertices[i]);
+      this.face.add(mesh);
+      return mesh;
+    });
+
+    // this.nodes.forEach((node) => {
+    //   node.weights.forEach((w) => {
+    //     if (w.i == 0 && w.w > 0.5) {
+    //       console.log(node);
+    //     }
+    //   });
+    // });
+
+    // let fp = this.featurePoints[0];
+    // fp.position.x = this.nodes[fp.vertexIndex].position.x - 0.5;
+    // this.update();
   }
+
 
   findNearestIndex(vertices, target) {
     let distance = Number.MAX_VALUE;
@@ -137,6 +177,7 @@ class App {
     // console.log(index, Math.round(Math.sqrt(distance) * 10000) / 10000);
     return index;
   }
+
 
   buildConnectionData(geometry) {
     this.nodes = geometry.vertices.map((v, i) => new Node(i, v.clone()));
@@ -157,15 +198,16 @@ class App {
     });
   }
 
+
   calcDistanceToFeaturePoint(index) {
-    this.nodes.forEach(node => node.distanceToFP[index] = Number.MAX_VALUE);
+    this.nodes.forEach((node) => node.distanceToFP[index] = Number.MAX_VALUE);
     let start = this.nodes[this.featurePointIndices[index]];
     start.distanceToFP[index] = 0;
     let processing = [start];
     while (processing.length) {
       let next = [];
-      processing.forEach(node => {
-        node.connection.forEach(connected => {
+      processing.forEach((node) => {
+        node.connection.forEach((connected) => {
           let distance = node.distanceToFP[index] + node.position.distanceTo(connected.position);
           if (distance < connected.distanceToFP[index]) {
             connected.distanceToFP[index] = distance;
@@ -176,6 +218,7 @@ class App {
       processing = next;
     }
   }
+
 
   calcWeightForNode(node) {
     // console.log('node', node);
@@ -192,8 +235,8 @@ class App {
       let node = this.nodes[index].position.clone().sub(fp1.position);
       let angle = p.angleTo(node);
       // console.log(i, index, node, angle, THREE.Math.radToDeg(angle));
-      return {index: i, angle: angle};
-    }).filter(a => {
+      return {index: i, angle};
+    }).filter((a) => {
       return !isNaN(a.angle) && a.angle < Math.PI / 2;
     }).sort((a, b) => a.angle - b.angle);
     // angles.forEach((a, i) => console.log(i, a));
@@ -218,9 +261,9 @@ class App {
       node.weights = this.featurePointIndices.map((id, i) => i == nearest ? 1 : 0);
     } else {
       const HALF_PI = Math.PI / 2;
-      node.weights = this.featurePointIndices.map(i => 0);
+      node.weights = [];
       node.weights[nearest] = Math.max(0, Math.sin(HALF_PI * (1.0 - node.distanceToFP[nearest] / d)));
-      angles.forEach(a => {
+      angles.forEach((a) => {
         if (node.distanceToFP[a.index] < d) {
           node.weights[a.index] = Math.sin(HALF_PI * (1.0 - node.distanceToFP[a.index] / d));
         }
@@ -228,25 +271,76 @@ class App {
     }
 
     node.weights = node.weights.map((w, i) => {
-      return {i: i, w: w};
-    }).sort((a, b) => b.w - a.w).filter(w => w.w > 0);
+      return {i, w};
+    }).sort((a, b) => b.w - a.w).filter((w) => w.w > 0);
 
     // node.weights.forEach(w => {
     //   console.log(w);
     // });
   }
 
-  animate() {
+
+  update() {
+    let vertices = this.face.geometry.vertices;
+
+    let displacement = this.featurePoints.map((mesh) => {
+      if (!mesh) return;
+      let node = this.nodes[mesh.vertexIndex];
+      // console.log(i, mesh.position.clone().sub(node.position));
+      return mesh.position.clone().sub(node.position);
+    });
+    this.nodes.forEach((target) => {
+    // let target = this.nodes[327];
+      if (target.weights.length == 0) return;
+      if (target.weights.length == 1) {
+        let w = target.weights[0];
+        // console.log(target.index, this.featurePointIndices[w.i], w);
+        vertices[target.index].copy(target.position).add(displacement[w.i].clone().multiplyScalar(w.w));
+      } else {
+        let a = new THREE.Vector3();
+        let b = 0;
+        target.weights.forEach((w) => {
+        // console.log(w, displacement[w.i], target.distanceToFP[w.i]);
+          let dp = displacement[w.i].clone().multiplyScalar(w.w);
+          let dist = 1.0 / (target.distanceToFP[w.i] * target.distanceToFP[w.i]);
+        // console.log(dp, dist);
+          a.add(dp.multiplyScalar(dist));
+          b += w.w * dist;
+        });
+        // console.log(a, b, target.weights.length);
+        a.multiplyScalar(1 / b);
+        // console.log(a);
+        vertices[target.index].copy(target.position).add(a);
+      }
+    });
+
+    this.face.geometry.verticesNeedUpdate = true;
+  }
+
+
+  animate(t) {
     requestAnimationFrame(this.animate);
+
     this.controls.update();
+
+    this.featurePoints.forEach((fp) => {
+      if (fp) {
+        // let v = this.nodes[fp.vertexIndex];
+        // fp.position.x = v.position.x + Math.sin(t / 500 + v.position.y) * 0.5;
+      }
+    });
+    this.update();
+
     this.renderer.render(this.scene, this.camera);
   }
+
 
   onResize() {
     this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
+
 
   onClick(e) {
     e.preventDefault();
