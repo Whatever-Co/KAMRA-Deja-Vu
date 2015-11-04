8537cdd7ecad7bf5e110b339042ecb54c19f1b0c
Saqoosha <a@saqoo.sh>
2015-11-05 01:56:40 +0900

    Add favicons etc..

--- main/public/sp/main.js
+++ main/public/sp/main.js
@@ -1,1 +1,50 @@
-!function(o){function e(n){if(r[n])return r[n].exports;var t=r[n]={exports:{},id:n,loaded:!1};return o[n].call(t.exports,t,t.exports,e),t.loaded=!0,t.exports}var r={};return e.m=o,e.c=r,e.p="",e(0)}([function(o,e){console.log("aoeuaoeuaoeuaoeuao")}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	
+
+/***/ }
+/******/ ]);
--- main/src/pc/main.js
+++
@@ -1,4 +0,0 @@
-/* global process */
-console.log('hoge???')
-console.log('u')
-console.log(process.env.NODE_ENV)
