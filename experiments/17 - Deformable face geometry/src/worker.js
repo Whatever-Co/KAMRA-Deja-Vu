07677c19eb5b545abf862b273735ecb06815b64a
Saqoosha <a@saqoo.sh>
2015-11-07 14:41:24 +0900

    [exp/17] Use quadtree to improve the performance
    そなにはやくなってない。。

--- experiments/17 - Deformable face geometry/src/worker.js
+++ experiments/17 - Deformable face geometry/src/worker.js
@@ -2,6 +2,7 @@ self.THREE = {}
 importScripts('libs/three.js')
 
 import Delaunay from 'delaunay-fast'
+import QuadTree from 'simple-quadtree'
 
 import StandardFaceData from './standardfacedata'
 
@@ -21,7 +22,58 @@ const convertData = (vertices) => {
 
   let triangleIndices = Delaunay.triangulate(standardFacePoints)
 
-  const getTriangleIndex = (p, vertices) => {
+  // let tree = QuadTree(-1, -1, 2, 2)
+  // tree.put({x: 0, y: 0, w: 0, h: 0, string: 'test'})
+  // console.table(tree.get({x:0, y: 0, w: 0.01, h: 0.01}))
+
+  //*
+  let tree = QuadTree(-1, -1, 2, 2)
+  for (let i = 0; i < triangleIndices.length; i += 3) {
+    let v0 = standardFacePoints[triangleIndices[i]]
+    let v1 = standardFacePoints[triangleIndices[i + 1]]
+    let v2 = standardFacePoints[triangleIndices[i + 2]]
+    let minX = Math.min(v0[0], v1[0], v2[0])
+    let minY = Math.min(v0[1], v1[1], v2[1])
+    let maxX = Math.max(v0[0], v1[0], v2[0])
+    let maxY = Math.max(v0[1], v1[1], v2[1])
+    tree.put({
+      x: minX,
+      y: minY,
+      w: maxX - minX,
+      h: maxY - minY,
+      index: i,
+      v0,
+      v1,
+      v2      
+    })
+  }
+  // console.log(JSON.parse(tree.stringify()))
+  console.log(tree.root)
+
+  // tree.put({x: 0, y: 0, w: 0.001, h: 0.001, string: 'test'})
+  // console.table(tree.get({x:-0.001, y: -0.001, w: 0.1, h: 0.1}))
+
+  // console.log(tree.get({x: 0, y: 0, w: 0.1, h: 0.1}))
+  //*/
+
+  // console.table(tree.get({x: -0.25, y: -0.25, w: 0.5, h: 0.5}))
+  // console.log(triangleIndices.length / 3)
+
+
+  const getTriangleIndex = (p) => {
+    let candidate = tree.get({x: p[0], y: p[1], w: 0, h: 0})
+    for (let i = 0; i < candidate.length; i++) {
+      let node = candidate[i]
+      let uv = Delaunay.contains([node.v0, node.v1, node.v2], p)
+      if (uv) {
+        uv.unshift(1 - uv[0] - uv[1])
+        return [node.index, uv]
+      }
+    }
+    console.error('not found')
+  }
+
+  const getTriangleIndex_ = (p, vertices) => {
     for (let i = 0; i < triangleIndices.length; i += 3) {
       let uv = Delaunay.contains([vertices[triangleIndices[i]], vertices[triangleIndices[i + 1]], vertices[triangleIndices[i + 2]]], p)
       if (uv) {
@@ -30,6 +82,20 @@ const convertData = (vertices) => {
       }
     }
   }
+
+  /**
+  let p = [vertices[0][0], vertices[0][1]]
+  console.table(p)
+  {
+    let [index, uvw] = getTriangleIndex(p, standardFacePoints)
+    console.log(index, standardFacePoints[triangleIndices[index]], standardFacePoints[triangleIndices[index + 1]], standardFacePoints[triangleIndices[index + 2]], uvw)
+  }
+  {
+    let [index, uvw] = getTriangleIndex_(p, standardFacePoints)
+    console.log(index, standardFacePoints[triangleIndices[index]], standardFacePoints[triangleIndices[index + 1]], standardFacePoints[triangleIndices[index + 2]], uvw)
+  }
+  // return
+  */
 
   return vertices.map((vertices) => {
     let weights = []
