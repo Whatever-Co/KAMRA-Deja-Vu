10839cee6f27fb497ba8047051cf362f6dff84cf
Saqoosha <a@saqoo.sh>
2015-11-12 14:58:51 +0900

    [exp/23] Draw eyes on to the captured texture

--- experiments/23 - Fukuwarai shader/src/main.js
+++ experiments/23 - Fukuwarai shader/src/main.js
@@ -46,73 +46,89 @@ class App {
   initObjects() {
     let loader = new createjs.LoadQueue()
     loader.loadManifest([
+      // {id: 'color', src: 'faceuv.png'},
       {id: 'lut', src: 'lut4b.png'},
+      // {id: 'image', src: 'media/shutterstock_56254417.png'},
+      // {id: 'data', src: 'media/shutterstock_56254417.json'}
     ])
     loader.on('complete', () => {
       let geometry = new DeformableFaceGeometry()
       let uvTexture = new DeformedUVTexture(this.renderer, geometry)
       geometry.addAttribute('uv2', uvTexture.uvAttribute)
 
-      let mesh = new THREE.Mesh(new THREE.PlaneGeometry(200, 200), new THREE.MeshBasicMaterial({map: uvTexture}))
-      mesh.position.set(-340, -150, 10)
-      this.scene.add(mesh)
-
       this.webcam = new WebcamPlane(this.camera)
       let scale = Math.tan(THREE.Math.degToRad(this.camera.fov / 2)) * this.camera.position.z * 2
       this.webcam.scale.set(scale, scale, scale)
       this.scene.add(this.webcam)
       this.webcam.start()
-      // this.webcam.visible = false
 
-      let material = new THREE.ShaderMaterial({
-        uniforms: {
-          map: {type: 't', value: this.webcam.texture},
-          lut: {type: 't', value: new THREE.CanvasTexture(loader.getResult('lut'))},
-          hoge: {type: 't', value: uvTexture},
-        },
-        vertexShader: `
-          attribute vec2 uv2;
+      let target = new THREE.WebGLRenderTarget(1024, 1024)
+      {
+        target.scene = new THREE.Scene()
+        let plane = this.webcam.clone()
+        plane.material.map = this.webcam.texture
+        plane.scale.copy(this.webcam.scale)
+        target.scene.add(plane)
+        let material = new THREE.ShaderMaterial({
+          uniforms: {
+            map: {type: 't', value: this.webcam.texture},
+            lut: {type: 't', value: new THREE.CanvasTexture(loader.getResult('lut'))},
+            hoge: {type: 't', value: uvTexture},
+          },
+          vertexShader: `
+            attribute vec2 uv2;
+            varying vec2 vUv;
+            varying vec2 vUv2;
+            void main() {
+              gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+              vUv = uv;
+              vUv2 = uv2;
+            }
+          `,
+          fragmentShader: `
+            uniform sampler2D map;
+            uniform sampler2D lut;
+            uniform sampler2D hoge;
 
-          varying vec2 vUv;
-          varying vec2 vUv2;
+            varying vec2 vUv;
+            varying vec2 vUv2;
 
-          void main() {
-            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-            vUv = uv;
-            vUv2 = uv2;
-          }
-        `,
-        fragmentShader: `
-          uniform sampler2D map;
-          uniform sampler2D lut;
-          uniform sampler2D hoge;
+            void main() {
+              vec4 original = texture2D(map, vUv);
+              vec4 uv2 = texture2D(lut, vUv2);
+              vec4 uv3 = texture2D(hoge, uv2.xy);
+              vec4 parts = texture2D(map, uv3.xy);
+              gl_FragColor = vec4(mix(original.rgb, parts.rgb, uv2.a), 1.0);
+            }
+          `,
+          transparent: true,
+        })
+        this.face = new THREE.Mesh(geometry, material)
+        this.face.matrixAutoUpdate = false
+        target.scene.add(this.face)
+      }
+      this.target = target
 
-          varying vec2 vUv;
-          varying vec2 vUv2;
+      let mesh = new THREE.Mesh(new THREE.PlaneGeometry(200, 200), new THREE.MeshBasicMaterial({map: target}))
+      mesh.position.set(-340, -150, 10)
+      this.scene.add(mesh)
 
-          void main() {
-            vec4 original = texture2D(map, vUv);
-            vec4 uv2 = texture2D(lut, vUv2);
-            vec4 uv3 = texture2D(hoge, uv2.xy);
-            vec4 parts = texture2D(map, uv3.xy);
-            gl_FragColor = vec4(mix(original.rgb, parts.rgb, uv2.a), 1.0);
-          }
-        `,
-        transparent: true,
-      })
-      this.face = new THREE.Mesh(geometry, material)
-      this.face.matrixAutoUpdate = false
-      this.face.scale.set(300, 300, 300)
-      // this.face.updateMatrixWorld()
-      this.scene.add(this.face)
+      this.face2 = new THREE.Mesh(this.face.geometry, new THREE.MeshBasicMaterial({map: target, transparent: true}))
+      this.face2.matrixAutoUpdate = false
+      this.scene.add(this.face2)
 
       this._updateObjects = () => {
         if (this.webcam.normalizedFeaturePoints) {
-          // this.face.geometry.deform(this.webcam.normalizedFeaturePoints)
           this.face.geometry.init(this.webcam.rawFeaturePoints, 320, 180, this.webcam.scale.y, 1700)
           this.face.matrix.copy(this.webcam.matrixFeaturePoints)
+          this.face2.matrix.copy(this.webcam.matrixFeaturePoints)
           uvTexture.update()
         }
+
+        let c = this.renderer.getClearColor()
+        this.renderer.setClearColor(0x888888)
+        this.renderer.render(this.target.scene, this.camera, this.target, true)
+        this.renderer.setClearColor(c)
       }
     })
   }
