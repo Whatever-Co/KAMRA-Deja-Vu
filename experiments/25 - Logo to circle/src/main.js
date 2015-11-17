c630a88c3d481692d935d91e4495f2f598615b1b
Saqoosha <a@saqoo.sh>
2015-11-17 14:55:45 +0900

    [exp/25] Implement previous version with Three.js (Line)

--- experiments/25 - Logo to circle/src/main.js
+++ experiments/25 - Logo to circle/src/main.js
@@ -2,290 +2,91 @@
 
 import $ from 'jquery'
 import 'jquery.transit'
-import _ from 'lodash'
 import 'OrbitControls'
-import TWEEN from 'tween.js'
-// import dat from 'dat-gui'
-import Snap from 'imports-loader?this=>window,fix=>module.exports=0!snapsvg'
-import {SpatialHash} from 'spatialhash'
-import {Noise} from 'noisejs'
-const noise = new Noise(Math.random())
+
+import 'shaders/CopyShader'
+import 'shaders/FXAAShader'
+import 'postprocessing/ShaderPass'
+import 'postprocessing/MaskPass'
+import 'postprocessing/RenderPass'
+import 'postprocessing/EffectComposer'
 
 import Config from './config'
 import Ticker from './ticker'
+import ParticledLogo from './particled-logo'
 
 import './main.sass'
 
 
 
-class Node extends THREE.Vector3 {
 
-  constructor() {
-    super()
-
-    this.linearVelocity = 3
-    this.velocity = new THREE.Vector3()
-    this.history = []
-    this.maxHistory = THREE.Math.randInt(20, 100)
-
-    this.target = new THREE.Vector3()
-    this.path = null
-    this.positionOnPath = 0
-    this.velocityOnPath = 3
-    this.maxSteeringSensitivity = 0.7
-
-    this.noiseStrength = 8
-    if (Math.random() < 0.1) {
-      this.noiseStrength = 30
-    }
-    if (Math.random() < 0.1) {
-      this.maxSteeringSensitivity = 0.1
-    }
-    this.variation = Math.random()
-  }
-
-
-  setPath(path, velocity = 3) {
-    if (path == this.path) {
-      return
-    }
-    this.path = path
-    this.positionOnPath = this.path.totalLength * Math.random()
-    this.velocityOnPath = velocity * THREE.Math.randFloat(0.9, 1.5)
-    this.distanceTraveledOnPath = 0
-  }
-
-
-  update(time) {
-    if (this.path) {
-      this.distanceTraveledOnPath += this.velocityOnPath
-      this.positionOnPath = (this.positionOnPath + this.velocityOnPath) % this.path.totalLength
-      if (this.positionOnPath < 0) {
-        this.positionOnPath += this.path.totalLength
-      }
-      let p = this.path.getPointAtLength2(this.positionOnPath)
-      this.target.x = p.x + noise.simplex2(this.x * 0.0001 + time + this.variation, this.y * 0.0001) * this.noiseStrength
-      this.target.y = p.y + noise.simplex2(this.x * 0.0001, this.y * 0.0001 - time) * this.noiseStrength
-
-      _v1.copy(this.target).sub(this)
-      let d = _v1.length()
-      let alpha = THREE.Math.mapLinear(THREE.Math.clamp(d, 0, 400), 0, 400, this.maxSteeringSensitivity, 0.05)
-      let a = signedAngle(this.velocity, _v1) * alpha
-
-      this.linearVelocity += (Math.max(Math.abs(this.velocityOnPath) * 0.9, d * 0.04) - this.linearVelocity) * 0.9
-      this.velocity.applyAxisAngle(PLUS_Z, a).setLength(this.linearVelocity)
-    }
-    this.add(this.velocity)
-
-    this.history.push([this.x, this.y])
-    if (this.history.length > this.maxHistory) {
-      this.history.shift()
-    }
-  }
-
-}
-
-
-const signedAngle = (v1, v2) => Math.atan2(v1.x * v2.y - v1.y * v2.x, v1.x * v2.x + v1.y * v2.y)
-
-const PLUS_Z = new THREE.Vector3(0, 0, 1)
-let _v1 = new THREE.Vector3()
 
 class App {
 
   constructor() {
-    this.canvas = document.createElement('canvas')
-    this.canvas.width = Config.RENDER_WIDTH
-    this.canvas.height = Config.RENDER_HEIGHT
-    document.body.appendChild(this.canvas)
-    this.ctx = this.canvas.getContext('2d')
-    this.ctx.fillStyle = 'rgb(26, 43, 52)'
-    this.ctx.fillRect(0, 0, Config.RENDER_WIDTH, Config.RENDER_HEIGHT)
-
-    this.logoPaths = Snap.parse(require('raw!./logo2.svg')).selectAll('path')
-    this.circlePath = Snap.parse(require('raw!./face-circle.svg')).select('path')
-    this.circlePath.totalLength = this.circlePath.getTotalLength()
-
-    console.time('prepare')
-    this.pathPoints = []
-    this.logoPaths.forEach((path) => {
-      path.totalLength = path.getTotalLength()
-      let n = Math.round(path.totalLength / 10)
-      let l = path.totalLength / n
-      for (let i = 0; i < n; i++) {
-        let p = path.getPointAtLength(i * l)
-        p.path = path
-        this.pathPoints.push(p)
-      }
-    })
-    let preparePath = (path) => {
-      let _points = []
-      let n = Math.floor(path.totalLength / 3)
-      for (let i = 0; i <= n; i++) {
-        let l = i / n * path.totalLength
-        let p = path.getPointAtLength(l)
-        p.length = l
-        _points.push(p)
-      }
-      path.getPointAtLength2 = (length) => {
-        if (length <= 0) {
-          _points[0]
-        }
-        if (length >= path.totalLength) {
-          return _.last(_points)
-        }
-        let r = 0
-        let l = _points.length - 1
-        let m = (l + r) >> 1
-        while (l - r > 1) {
-          if (_points[m].length < length) {
-            r = m
-          } else {
-            l = m
-          }
-          m = (l + r) >> 1
-        }
-        return _points[m]
-      }
-    }
-    this.logoPaths.forEach((path) => preparePath(path))
-    preparePath(this.circlePath)
-    console.timeEnd('prepare')
-
-    this.nodes = []
-    let center = new THREE.Vector3(Config.RENDER_WIDTH / 2, Config.RENDER_HEIGHT / 2, 0)
-    for (let i = 0; i < 100; i++) {
-      let node = new Node()
-      if (Math.random() < 0.5) {
-        node.x = Math.random() < 0.5 ? -100 : Config.RENDER_WIDTH + 100
-        node.y = THREE.Math.randFloat(-100, Config.RENDER_HEIGHT + 100)
-      } else {
-        node.x = THREE.Math.randFloat(-100, Config.RENDER_WIDTH + 100)
-        node.y = Math.random() < 0.5 ? -100 : Config.RENDER_HEIGHT + 100
-      }
-      node.velocity.copy(node).sub(center).setLength(3)
-      node.visible = false
-      this.nodes.push(node)
-    }
-    {
-      let paths = _.shuffle(this.pathPoints).slice(0, this.nodes.length - this.logoPaths.length).map((p) => p.path)
-      paths.push(...this.logoPaths)
-      paths = _.shuffle(paths)
-      this.nodes.forEach((node, i) => {
-        node.setPath(paths[i], Math.random() < 0.5 ? -3 : 3)
-      })
-    }
-
-    {
-      let i = 0
-      let interval = setInterval(() => {
-        this.nodes[i].visible = true
-        if (++i == this.nodes.length) {
-          clearInterval(interval)
-        }
-      }, 10)
-    }
-
-    window.addEventListener('resize', this.onResize.bind(this))
-    this.onResize()
-
-    this.circleMode = false
-    window.addEventListener('keydown', (e) => {
-      if (e.keyCode == 32) {
-        this.circleMode = !this.circleMode
-        if (this.circleMode) {
-          this.nodes.forEach((node) => {
-            node.setPath(this.circlePath, THREE.Math.randFloat(10, 20) / 3)
-          })
-        } else {
-          let paths = _.shuffle(this.pathPoints).slice(0, this.nodes.length - this.logoPaths.length).map((p) => p.path)
-          paths.push(...this.logoPaths)
-          paths = _.shuffle(paths)
-          this.nodes.forEach((node, i) => {
-            node.setPath(paths[i], Math.random() < 0.5 ? -3 : 3)
-          })
-        }
-      }
-    })
-    // window.addEventListener('keyup', (e) => {
-    //   if (e.keyCode == 32) {
-    //     _.shuffle(this.logoPaths).map((p, i) => {
-    //       this.nodes[i].setPath(p)
-    //     })
-    //   }
-    // })
+    this.initScene()
+    this.initObjects()
 
     Ticker.on('update', this.animate.bind(this))
     Ticker.start()
   }
 
 
-  animate(f, t) {
-    TWEEN.update(t)
-    this.update(f, t)
-    this.update(f, t)
-    this.update(f, t)
-    this.draw(f, t)
+  initScene() {
+    this.camera = new THREE.PerspectiveCamera(16.8145, Config.RENDER_WIDTH / Config.RENDER_HEIGHT, 10, 10000)
+    this.camera.position.set(0, 0, 1700)
+
+    this.scene = new THREE.Scene()
+
+    this.renderer = new THREE.WebGLRenderer({antialias: false})
+    this.renderer.setClearColor(0x071520)
+    this.renderer.setSize(Config.RENDER_WIDTH, Config.RENDER_HEIGHT)
+    document.body.appendChild(this.renderer.domElement)
+
+    this.composer = new THREE.EffectComposer(this.renderer)
+    this.composer.addPass(new THREE.RenderPass(this.scene, this.camera))
+    let effect = new THREE.ShaderPass(THREE.FXAAShader)
+    effect.uniforms.resolution.value.set(1 / Config.RENDER_WIDTH, 1 / Config.RENDER_HEIGHT)
+    effect.renderToScreen = true
+    this.composer.addPass(effect)
+
+    this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement)
+
+    window.addEventListener('resize', this.onResize.bind(this))
+    this.onResize()
   }
 
 
-  lerp(p0, p1, a) {
-    return p0 + (p1 - p0) * a
-  }
+  initObjects() {
+    this.logo = new ParticledLogo()
+    let height = Math.tan(THREE.Math.degToRad(this.camera.fov / 2)) * this.camera.position.z * 2
+    console.log(height)
+    this.logo.position.set(height / 9 * 16 / -2, height / 2, 0)
+    let scale = height / 1080
+    this.logo.scale.set(scale, -scale, scale)
+    this.scene.add(this.logo)
 
-
-  update(f, t) {
-    t *= 0.001
-    this.nodes.forEach((node) => {
-      if (node.visible) {
-        node.update(t)
-        if (!this.circleMode && Math.random() < (node.distanceTraveledOnPath - node.path.totalLength * 3) / 15000) {
-          if (this.freePath) {
-            let prev = node.path
-            node.setPath(this.freePath)
-            this.freePath = prev
-          } else {
-            this.freePath = node.path
-            node.setPath(_.sample(this.logoPaths))
-          }
-        }
+    window.addEventListener('keydown', (e) => {
+      if (e.keyCode == 32) {
+        console.log(this.logo.mode)
+        this.logo.setMode(this.logo.mode == 'logo' ? 'circle' : 'logo')
       }
     })
   }
 
 
-  draw() {
-    let ctx = this.ctx
+  animate(currentFrame, time) {
+    this.logo.update(currentFrame, time)
 
-    ctx.save()
-    // ctx.globalAlpha = 0.1
-    ctx.fillStyle = 'rgb(26, 43, 52)'
-    ctx.fillRect(0, 0, Config.RENDER_WIDTH, Config.RENDER_HEIGHT)
-    ctx.restore()
-
-    ctx.save()
-    this.nodes.forEach((node) => {
-      ctx.strokeStyle = '#ffffff'
-      for (let i = 0; i < node.history.length - 1; i++) {
-        ctx.globalAlpha = i / node.maxHistory
-        ctx.beginPath()
-        ctx.moveTo(node.history[i][0], node.history[i][1])
-        ctx.lineTo(node.history[i + 1][0], node.history[i + 1][1])
-        ctx.stroke()
-      }
-      // ctx.strokeStyle = 'rgb(0, 255, 0)'
-      // ctx.beginPath()
-      // ctx.arc(node.x, node.y, 5, 0, Math.PI * 2, false)
-      // ctx.stroke()
-    })
-    ctx.restore()
-
+    this.controls.update()
+    // this.renderer.render(this.scene, this.camera)
+    this.composer.render()
   }
 
 
   onResize() {
     let s = Math.max(window.innerWidth / Config.RENDER_WIDTH, window.innerHeight / Config.RENDER_HEIGHT)
-    $(this.canvas).css({
+    $(this.renderer.domElement).css({
       transformOrigin: 'left top',
       translate: [(window.innerWidth - Config.RENDER_WIDTH * s) / 2, (window.innerHeight - Config.RENDER_HEIGHT * s) / 2],
       scale: [s, s],
