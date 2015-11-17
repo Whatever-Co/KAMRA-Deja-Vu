17b17c67b7c4eb75d78c1663a55c7fa1bc5d3f19
Saqoosha <a@saqoo.sh>
2015-11-17 20:11:01 +0900

    Add fade-in/out to page transition

--- main/src/pc/page-manager.js
+++ main/src/pc/page-manager.js
@@ -1,5 +1,6 @@
 import {EventEmitter} from 'events'
 import $ from 'jquery'
+import StateMachine from 'javascript-state-machine'
 
 
 export default class PageManager extends EventEmitter {
@@ -14,25 +15,34 @@ export default class PageManager extends EventEmitter {
   init() {
     // loading
     this.stateMachine.onleaveloadAssets = () => {
-      $('#loading').hide()
+      $('#loading').fadeOut(1000, () => {
+        this.stateMachine.transition()
+      })
+      return StateMachine.ASYNC
     }
 
     // top
     this.stateMachine.onentertop = () => {
-      $('#top').show()
+      $('#top').fadeIn(1000)
     }
     this.stateMachine.onleavetop = () => {
-      $('#top').hide() 
+      $('#top').fadeOut(1000, () => {
+        this.stateMachine.transition()
+      })
+      return StateMachine.ASYNC
     }
     $('.with-webcam').click(() => this.stateMachine.start(true))
     $('.without-webcam').click(() => this.stateMachine.start(false))
 
     // share
     this.stateMachine.onentershare = () => {
-      $('#share').show()
+      $('#share').fadeIn(1000)
     }
     this.stateMachine.onleaveshare = () => {
-      $('#share').hide()
+      $('#share').fadeOut(1000, () => {
+        this.stateMachine.transition()
+      })
+      return StateMachine.ASYNC
     }
     $('.button-top').click(() => this.stateMachine.goTop())
   }
