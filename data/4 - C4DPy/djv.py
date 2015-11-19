ccda0f1a6301c60abdd6b2303a0e9ee0c80113a0
baku89 <mail@baku89.com>
2015-11-19 14:22:28 +0900

    [data] Add mouth polygons to eyemouth

--- data/4 - C4DPy/djv.py
+++ data/4 - C4DPy/djv.py
@@ -49,6 +49,12 @@ fps = doc[c4d.DOCUMENT_FPS]
 
 projDir = os.path.normpath(doc.GetDocumentPath() + "/../")
 
+mouthVertexIndices = [56, 57, 58, 59, 62, 65, 176, 182, 183, 230, 231, 232, 234, 236, 338, 341]
+mouthPolygons = [
+    [5,8,7], [4,5,7], [4,7,6], [2,4,6], [2,6,1], [3,2,1], [3,0,1],
+    [5,15,8], [13,15,5], [13,14,15], [11,14,13], [11,10,14], [12,10,11], [12,9,10]
+]
+
 #========================================
 def search(name):
     return doc.SearchObject(name)
