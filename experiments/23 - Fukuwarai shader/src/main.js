d66cc075d497530ba538798e052cbc53774ce425
Saqoosha <a@saqoo.sh>
2015-11-23 02:13:03 +0900

    [exp/23] Sample lut texture with blur
    あんまし効果ない

--- experiments/23 - Fukuwarai shader/src/main.js
+++ experiments/23 - Fukuwarai shader/src/main.js
@@ -47,7 +47,7 @@ class App {
     let loader = new createjs.LoadQueue()
     loader.loadManifest([
       // {id: 'color', src: 'faceuv.png'},
-      {id: 'lut', src: 'lut5.png'},
+      {id: 'lut', src: 'remap_1.png'},
       // {id: 'image', src: 'media/shutterstock_56254417.png'},
       // {id: 'data', src: 'media/shutterstock_56254417.json'}
     ])
@@ -73,7 +73,7 @@ class App {
           uniforms: {
             map: {type: 't', value: this.webcam.texture},
             lut: {type: 't', value: new THREE.CanvasTexture(loader.getResult('lut'))},
-            hoge: {type: 't', value: uvTexture},
+            uvTexture: {type: 't', value: uvTexture},
           },
           vertexShader: `
             attribute vec2 uv2;
@@ -88,17 +88,51 @@ class App {
           fragmentShader: `
             uniform sampler2D map;
             uniform sampler2D lut;
-            uniform sampler2D hoge;
+            uniform sampler2D uvTexture;
 
             varying vec2 vUv;
             varying vec2 vUv2;
 
+            vec4 sampleLUT(vec2 uv) {
+              const int NUM_TAPS = 12;
+            
+              vec2 fTaps_Poisson[NUM_TAPS];
+              fTaps_Poisson[0]  = vec2(-.326,-.406);
+              fTaps_Poisson[1]  = vec2(-.840,-.074);
+              fTaps_Poisson[2]  = vec2(-.696, .457);
+              fTaps_Poisson[3]  = vec2(-.203, .621);
+              fTaps_Poisson[4]  = vec2( .962,-.195);
+              fTaps_Poisson[5]  = vec2( .473,-.480);
+              fTaps_Poisson[6]  = vec2( .519, .767);
+              fTaps_Poisson[7]  = vec2( .185,-.893);
+              fTaps_Poisson[8]  = vec2( .507, .064);
+              fTaps_Poisson[9]  = vec2( .896, .412);
+              fTaps_Poisson[10] = vec2(-.322,-.933);
+              fTaps_Poisson[11] = vec2(-.792,-.598);
+
+              vec4 sum;
+              float size = 2. / 256.;
+              for (int i = 0; i < NUM_TAPS; i++) {
+                // sum += texture2D(remap, (gl_FragCoord.xy + fTaps_Poisson[i] * blurSize) / resolution);
+                sum += texture2D(lut, uv + fTaps_Poisson[i] * size);
+              }
+              sum /= float(NUM_TAPS);
+
+              // vec4 uv2 = texture2D(lut, uv);
+              vec4 uv2 = sum;
+              // uv2.rg = mix(uv2.rg, uv, 1. - uv2.a);
+              // uv2.a = texture2D(lut, uv).a;
+              return uv2;
+            }
+
             void main() {
               vec4 original = texture2D(map, vUv);
-              vec4 uv2 = texture2D(lut, vUv2);
-              vec4 uv3 = texture2D(hoge, uv2.xy);
+              // vec4 uv2 = texture2D(lut, vUv2);
+              vec4 uv2 = sampleLUT(vUv2);
+              vec4 uv3 = texture2D(uvTexture, uv2.xy);
               vec4 parts = texture2D(map, uv3.xy);
               gl_FragColor = vec4(mix(original.rgb, parts.rgb, uv2.a), 1.0);
+              // gl_FragColor = parts;
             }
           `,
           transparent: true,
@@ -109,9 +143,37 @@ class App {
       }
       this.target = target
 
-      let mesh = new THREE.Mesh(new THREE.PlaneGeometry(200, 200), new THREE.MeshBasicMaterial({map: target}))
-      mesh.position.set(-340, -150, 10)
-      this.scene.add(mesh)
+      {
+        // let material = new THREE.MeshBasicMaterial({map: this.face.material.uniforms.lut.value})
+        let material = new THREE.ShaderMaterial({
+          uniforms: this.face.material.uniforms,
+          vertexShader: `
+            varying vec2 vUv;
+            void main() {
+              gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+              vUv = uv;
+            }
+          `,
+          fragmentShader: `
+            uniform sampler2D map;
+            uniform sampler2D lut;
+            uniform sampler2D uvTexture;
+            varying vec2 vUv;
+            void main() {
+              gl_FragColor = texture2D(lut, vUv);
+              gl_FragColor.rg = mix(gl_FragColor.rg, vUv, 1. - gl_FragColor.a);
+              // gl_FragColor.a = 1.;
+            }
+          `,
+          depthTest: false,
+          depthWrite: false,
+          transparent: true,
+        })
+        let mesh = new THREE.Mesh(new THREE.PlaneGeometry(200, 200), material)
+        mesh.renderOrder = 1000
+        mesh.position.set(-340, -150, 10)
+        this.scene.add(mesh)
+      }
 
       this.face2 = new THREE.Mesh(this.face.geometry, new THREE.MeshBasicMaterial({map: target, transparent: true}))
       this.face2.matrixAutoUpdate = false
