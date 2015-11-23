e53fba064cc08df29d3351f221aeddc36e36e083
Saqoosha <a@saqoo.sh>
2015-11-24 08:44:59 +0900

    Combine post-effect shaders
    あと noise も shader 側で。

--- main/src/pc/post-effects/chromatic-aberration.js
+++ main/src/pc/post-effects/chromatic-aberration.js
@@ -1,18 +1,15 @@
 /* global THREE */
 
-import Config from '../config'
-
 
 export default class ChromaticAberration extends THREE.ShaderPass {
 
-  constructor(lut) {
+  constructor() {
     super({
       uniforms: {
         tDiffuse: {type: 't', value: null},
-        resolution: {type: 'v2', value: new THREE.Vector2(Config.RENDER_WIDTH, Config.RENDER_HEIGHT)}
       },
       vertexShader: require('../shaders/basic-transform.vert'),
-      fragmentShader: require('../shaders/chromatic-aberration.frag')
+      fragmentShader: require('../shaders/chromatic-aberration.frag'),
     })
   }
 
