1c54121a99f0b8348853e669f18dae0870baf1d0
Saqoosha <a@saqoo.sh>
2015-11-24 06:44:49 +0900

    Append dummy extension to enable caching on cloudflare

--- main/src/pc/bgm-manager.js
+++ main/src/pc/bgm-manager.js
@@ -8,16 +8,17 @@ class BgmManager {
     this.playerB = document.createElement('audio')
   }
 
-  play(file, volume=1.0){
-    if(this.player) {
+  play(file, volume = 1.0) {
+    if (this.player) {
       return
     }
     this.masterVolume = volume
 
     let ext = '.mp3'
-    if(this.playerA.canPlayType("audio/ogg") != "") {
+    if (this.playerA.canPlayType('audio/ogg') != '') {
       ext = '.ogg'
     }
+    ext += '?.jpg'
 
     this.playerA.src = this.playerB.src  = file + ext
     this.player = this.playerA
@@ -25,7 +26,7 @@ class BgmManager {
   }
 
   stop() {
-    $(this.player).animate({volume: 0}, 3000, ()=> {
+    $(this.player).animate({volume: 0}, 3000, () => {
       this.player.pause()
     })
   }
@@ -38,12 +39,12 @@ class BgmManager {
   }
 
   _timeupdate(e) {
-    if(this.player.currentTime < this.player.duration - FADETIME) {
+    if (this.player.currentTime < this.player.duration - FADETIME) {
       return
     }
     this.player.removeEventListener('timeupdate', this._timeupdate.bind(this), false)
 
-    if(this.player == this.playerA) {
+    if (this.player == this.playerA) {
       this.player = this.playerB
     } else {
       this.player = this.playerA
