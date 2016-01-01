f0b46eb1e74b4adc87d2578c378e29a06fcedad1
baku89 <mail@baku89.com>
2016-01-01 16:46:20 +0900

    [data] Add keyframes.simple.js

---
+++ data/3 - JSON/keyframes.simple.js
@@ -0,0 +1,345 @@
+{
+    "i_extra": {
+        "property": {
+            "curl_rotation": [],
+            "curl_offset": [],
+            "turbulance": [],
+            "webcam_fade": [],
+            "curl_strength": [],
+            "scale_z": [],
+            "interpolation": []
+        },
+        "out_frame": 417,
+        "in_frame": 0
+    },
+    "user_children": {
+        "property": [
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": [],
+                "face_vertices": [],
+                "eyemouth_vertices": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": [],
+                "face_vertices": [],
+                "eyemouth_vertices": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": [],
+                "face_vertices": [],
+                "eyemouth_vertices": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": [],
+                "face_vertices": [],
+                "eyemouth_vertices": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": [],
+                "face_vertices": [],
+                "eyemouth_vertices": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": [],
+                "face_vertices": [],
+                "eyemouth_vertices": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": [],
+                "face_vertices": [],
+                "eyemouth_vertices": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": [],
+                "face_vertices": [],
+                "eyemouth_vertices": []
+            }
+        ],
+        "out_frame": 1766,
+        "in_frame": 837
+    },
+    "falling_children": {
+        "property": [
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "scale": []
+            }
+        ],
+        "out_frame": 3141,
+        "in_frame": 2512
+    },
+    "user": {
+        "property": {
+            "position": [],
+            "quaternion": [],
+            "scale": [],
+            "face_vertices": [],
+            "eyemouth_vertices": []
+        },
+        "out_frame": 3141,
+        "in_frame": 0
+    },
+    "user_particles": {
+        "property": [
+            {
+                "position": [],
+                "quaternion": [],
+                "mesh_index": [],
+                "enabled": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "mesh_index": [],
+                "enabled": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "mesh_index": [],
+                "enabled": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "mesh_index": [],
+                "enabled": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "mesh_index": [],
+                "enabled": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "mesh_index": [],
+                "enabled": []
+            },
+            {
+                "position": [],
+                "quaternion": [],
+                "mesh_index": [],
+                "enabled": []
+            }
+        ],
+        "out_frame": 2039,
+        "in_frame": 1347
+    },
+    "mosaic": {
+        "property": {
+            "time": []
+        },
+        "out_frame": 3355,
+        "in_frame": 2700
+    },
+    "slice_alt": {
+        "property": {
+            "face_vertices": [],
+            "eyemouth_vertices": []
+        },
+        "out_frame": 2119,
+        "in_frame": 1767
+    },
+    "falling_children_mesh": {
+        "property": [
+            {
+                "face_vertices": [],
+                "eyemouth_vertices": []
+            },
+            {
+                "face_vertices": [],
+                "eyemouth_vertices": []
+            }
+        ],
+        "out_frame": 2752,
+        "in_frame": 2512
+    },
+    "slice_col": {
+        "property": [
+            {
+                "position": [],
+                "quaternion": []
+            },
+            {
+                "position": [],
+                "quaternion": []
+            },
+            {
+                "position": [],
+                "quaternion": []
+            },
+            {
+                "position": [],
+                "quaternion": []
+            },
+            {
+                "position": [],
+                "quaternion": []
+            },
+            {
+                "position": [],
+                "quaternion": []
+            },
+            {
+                "position": [],
+                "quaternion": []
+            },
+            {
+                "position": [],
+                "quaternion": []
+            },
+            {
+                "position": [],
+                "quaternion": []
+            }
+        ],
+        "out_frame": 2119,
+        "in_frame": 1662
+    },
+    "slice_row": {
+        "property": [
+            {
+                "rotation": [],
+                "offset_x": []
+            },
+            {
+                "rotation": [],
+                "offset_x": []
+            },
+            {
+                "rotation": [],
+                "offset_x": []
+            },
+            {
+                "rotation": [],
+                "offset_x": []
+            },
+            {
+                "rotation": [],
+                "offset_x": []
+            }
+        ],
+        "out_frame": 2119,
+        "in_frame": 1662
+    },
+    "camera": {
+        "property": {
+            "focus_distance": [],
+            "position": [],
+            "quaternion": [],
+            "fov": []
+        },
+        "out_frame": 3820,
+        "in_frame": 0
+    },
+    "user_alt": {
+        "property": [
+            {
+                "scale": [],
+                "enabled": [],
+                "quaternion": [],
+                "eyemouth_vertices": [],
+                "position": [],
+                "face_vertices": []
+            },
+            {
+                "scale": [],
+                "enabled": [],
+                "quaternion": [],
+                "eyemouth_vertices": [],
+                "position": [],
+                "face_vertices": []
+            }
+        ],
+        "out_frame": 1620,
+        "in_frame": 1380
+    },
+    "o2_extra": {
+        "property": {
+            "webcam_fade": [],
+            "turbulance": [],
+            "interpolation": []
+        },
+        "out_frame": 3820,
+        "in_frame": 3356
+    }
+}
